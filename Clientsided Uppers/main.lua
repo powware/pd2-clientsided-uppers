e243ade09339434394387f7e7db64c10a086fb31
powware <m4chau3r@gmail.com>
2020-11-08 08:45:40 +0100

    v1.2 Code rewrite

--- Clientsided Uppers/main.lua
+++ Clientsided Uppers/main.lua
@@ -3,115 +3,51 @@ ClientsidedUppers._client_sided_faks = {}
 ClientsidedUppers._used_client_sided_faks = {}
 ClientsidedUppers.__index = ClientsidedUppers
 
-function ClientsidedUppers:Add(pos, rot, bits, min_distance, upgrade_lvl)
+-- adds a clientsided FAK to the auto_recovery list
+function ClientsidedUppers.Add(pos, min_distance, upgrade_lvl)
 	local fak = {}
-	setmetatable(fak, self)
-	fak.pos = pos
-	fak.rot = rot
-	fak.bits = bits
-	fak.min_distance = min_distance
-	fak.upgrade_lvl = upgrade_lvl
-
-	table.insert(ClientsidedUppers._client_sided_faks, fak)
-end
-
-function ClientsidedUppers.Remove(pos)
-	local closest_dst = -1
-	local closest = 0
-	for i, fak in pairs(ClientsidedUppers._client_sided_faks) do
-		local dst = mvector3.distance(pos, fak.pos)
-
-		if dst < closest_dst or closest_dst == -1 then
-			closest_dst = dst
-			closest = i
-		end
-	end
-	if closest >= 1 then
-		table.remove(ClientsidedUppers._client_sided_faks, closest)
-	end
-end
-
-function ClientsidedUppers.GetFirstAidKit(pos)
-	local closest_dst = -1
-	local closest = nil
-	for i, fak in pairs(ClientsidedUppers._client_sided_faks) do
-		local dst = mvector3.distance(pos, fak.pos)
-
-		if dst <= fak.min_distance and dst < closest_dst then
-			closest_dst = dst
-			closest = fak
-		end
-	end
-
-	return closest
-end
-
-function ClientsidedUppers.AddUsed(fak)
-	table.insert(ClientsidedUppers._used_client_sided_faks, fak)
-end
-
-function ClientsidedUppers.RemoveUsed(pos)
-	local closest_dst = -1
-	local closest = 0
-	for i, fak in pairs(ClientsidedUppers._used_client_sided_faks) do
-		local dst = mvector3.distance(pos, fak.pos)
-
-		if dst < closest_dst or closest_dst == -1 then
-			closest_dst = dst
-			closest = i
-		end
-		if dst <= 5 then
-			table.remove(ClientsidedUppers._used_client_sided_faks, i)
-		end
-	end
-	if closest >= 1 then
-		table.remove(ClientsidedUppers._used_client_sided_faks, closest)
-	end
-end
-
-function ClientsidedUppers.GetUsedFirstAidKit(pos)
-	local closest_dst = -1
-	local closest = nil
-	for i, fak in pairs(ClientsidedUppers._used_client_sided_faks) do
-		local dst = mvector3.distance(pos, fak.pos)
-
-		if dst < closest_dst or closest_dst == -1 then
-			closest_dst = dst
-			closest = fak
-		end
-	end
-
-	return closest
-end
-
-function ClientsidedUppers:Take(unit)
-	ClientsidedUppers.Remove(self.pos)
-	ClientsidedUppers.AddUsed(self)
-
-	unit:character_damage():band_aid_health()
-
-	managers.chat:_receive_message(
-		ChatManager.GAME,
-		"ClientsidedUppers",
-		"took clientsided FAK",
-		tweak_data.system_chat_color
+	setmetatable(fak, FirstAidKitBase)
+	fak._empty = false
+	fak._damage_reduction_upgrade = upgrade_lvl == 1
+	fak._clientsided = true
+	table.insert(
+		FirstAidKitBase.List,
+		{
+			obj = fak,
+			pos = pos,
+			min_distance = min_distance
+		}
 	)
-
-	if self.upgrade_lvl == 1 then
-		managers.player:activate_temporary_upgrade("temporary", "first_aid_damage_reduction")
-	end
 end
 
-function ClientsidedUppers:SyncUsage(unit)
-	if managers.network:session() then
-		managers.network:session():send_to_peers_synched("sync_unit_event_id_16", unit, "base", 2)
+-- removes the closest clientsided FAK from the autorecovery list
+-- if the fak is empty it's usage is synchronized
+function ClientsidedUppers.Remove(fak)
+	local pos = fak._unit:position()
+	local closest_dst = -1
+	local closest = 0
+	local closest_fak = nil
+	for i, o in pairs(FirstAidKitBase.List) do
+		local dst = mvector3.distance(pos, o.pos)
+
+		if o.obj._clientsided and (dst <= closest_dst or closest_dst == -1) then
+			closest_dst = dst
+			closest = i
+			closest_fak = o.obj
+		end
 	end
 
-	unit:_set_empty()
+	if closest >= 1 then
+		table.remove(FirstAidKitBase.List, closest)
+	end
+
+	return closest_fak
 end
 
 function ClientsidedUppers.Hooks()
 	if RequiredScript == "lib/units/beings/player/playerequipment" then
+		-- as a client instead of only sending a request to place a FAK
+		-- also add a clientsided FAK to the auto_recovery list
 		function PlayerEquipment:use_first_aid_kit()
 			local ray = self:valid_shape_placement("first_aid_kit")
 
@@ -133,7 +69,7 @@ function ClientsidedUppers.Hooks()
 				if Network:is_client() then
 					if auto_recovery == 1 then
 						local min_distance = tweak_data.upgrades.values.first_aid_kit.first_aid_kit_auto_recovery[1]
-						ClientsidedUppers:Add(pos, rot, bits, min_distance, upgrade_lvl)
+						ClientsidedUppers.Add(pos, min_distance, upgrade_lvl)
 					end
 
 					managers.network:session():send_to_host("place_deployable_bag", "FirstAidKitBase", pos, rot, bits)
@@ -146,130 +82,88 @@ function ClientsidedUppers.Hooks()
 
 			return false
 		end
-	elseif RequiredScript == "lib/units/beings/player/playerdamage" then
-		function PlayerDamage:_check_bleed_out(can_activate_berserker, ignore_movement_state)
-			if self:get_real_health() == 0 and not self._check_berserker_done then
-				if self._unit:movement():zipline_unit() then
-					self._bleed_out_blocked_by_zipline = true
+	elseif RequiredScript == "lib/units/equipment/first_aid_kit/firstaidkitbase" then
+		-- retrieves the closest fitting FAK for auto_recovery
+		-- if no synchronized FAK is fitting take the closest fitting clientsided FAK
+		-- if no clientsided FAK is fitting return nil
+		function FirstAidKitBase.GetFirstAidKit(pos)
+			local closest_dst = -1
+			local closest_fak = nil
+			local closest_clientsided_dst = -1
+			local closest_clientsided_fak = nil
+			for i, o in pairs(FirstAidKitBase.List) do
+				local dst = mvector3.distance(pos, o.pos)
 
-					return
-				end
-
-				if not ignore_movement_state and self._unit:movement():current_state():bleed_out_blocked() then
-					self._bleed_out_blocked_by_movement_state = true
-
-					return
-				end
-
-				local time = Application:time()
-
-				if not self._block_medkit_auto_revive and time > self._uppers_elapsed + self._UPPERS_COOLDOWN then
-					local auto_recovery_kit = FirstAidKitBase.GetFirstAidKit(self._unit:position())
-
-					if auto_recovery_kit then
-						auto_recovery_kit:take(self._unit)
-						self._unit:sound():play("pickup_fak_skill")
-
-						self._uppers_elapsed = time
-
-						return
-					else
-						local client_sided_fak = ClientsidedUppers.GetFirstAidKit(self._unit:position())
-						if client_sided_fak then
-							client_sided_fak:Take(self._unit)
-							self._unit:sound():play("pickup_fak_skill")
-
-							self._uppers_elapsed = time
-							return
-						end
+				if not o.obj._empty and dst <= o.min_distance then
+					if not o.obj._clientsided and (dst <= closest_dst or closest_dst == -1) then
+						closest_dst = dst
+						closest_fak = o.obj
+					elseif o.obj._clientsided and (dst <= closest_clientsided_dst or closest_clientsided_dst == -1) then
+						closest_clientsided_dst = dst
+						closest_clientsided_fak = o.obj
 					end
 				end
+			end
+			return closest_fak and closest_fak or closest_clientsided_fak
+		end
 
-				if can_activate_berserker and not self._check_berserker_done then
-					local has_berserker_skill = managers.player:has_category_upgrade("temporary", "berserker_damage_multiplier")
+		-- takes synchronized and clientsided FAKs
+		-- for clientsided FAKs it doesn't synchronize it's usage yet
+		-- they just get flagged as empty to be processed when they are synchronized
+		function FirstAidKitBase:take(unit)
+			if self._empty then
+				return
+			end
 
-					if has_berserker_skill and not self._disable_next_swansong then
-						managers.hud:set_teammate_condition(
-							HUDManager.PLAYER_PANEL,
-							"mugshot_swansong",
-							managers.localization:text("debug_mugshot_downed")
-						)
-						managers.player:activate_temporary_upgrade("temporary", "berserker_damage_multiplier")
+			unit:character_damage():band_aid_health()
 
-						self._current_state = nil
-						self._check_berserker_done = true
+			if self._damage_reduction_upgrade then
+				managers.player:activate_temporary_upgrade("temporary", "first_aid_damage_reduction")
+			end
 
-						if
-							alive(self._interaction:active_unit()) and
-								not self._interaction:active_unit():interaction():can_interact(self._unit)
-						 then
-							self._unit:movement():interupt_interact()
-						end
-
-						self._listener_holder:call("on_enter_swansong")
-					end
-
-					self._disable_next_swansong = nil
+			if self._clientsided then
+				managers.chat:_receive_message(
+					ChatManager.GAME,
+					"SYSTEM",
+					"Clientsided Uppers saved your life.",
+					tweak_data.system_chat_color
+				)
+				self._empty = true
+			else
+				if managers.network:session() then
+					managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "base", 2)
 				end
 
-				self._hurt_value = 0.2
-				self._damage_to_hot_stack = {}
-
-				managers.environment_controller:set_downed_value(0)
-				SoundDevice:set_rtpc("downed_state_progression", 0)
-
-				if not self._check_berserker_done or not can_activate_berserker then
-					self._revives = Application:digest_value(Application:digest_value(self._revives, false) - 1, true)
-					self._check_berserker_done = nil
-
-					managers.environment_controller:set_last_life(Application:digest_value(self._revives, false) <= 1)
-
-					if Application:digest_value(self._revives, false) == 0 then
-						self._down_time = 0
-					end
-
-					self._bleed_out = true
-					self._current_state = nil
-
-					managers.player:set_player_state("bleed_out")
-
-					self._critical_state_heart_loop_instance = self._unit:sound():play("critical_state_heart_loop")
-					self._slomo_sound_instance = self._unit:sound():play("downed_slomo_fx")
-					self._bleed_out_health =
-						Application:digest_value(
-						tweak_data.player.damage.BLEED_OUT_HEALTH_INIT *
-							managers.player:upgrade_value("player", "bleed_out_health_multiplier", 1),
-						true
-					)
-
-					self:_drop_blood_sample()
-					self:on_downed()
-				end
-			elseif not self._said_hurt and self:get_real_health() / self:_max_health() < 0.2 then
-				self._said_hurt = true
-
-				PlayerStandard.say_line(self, "g80x_plu")
+				self:_set_empty()
 			end
 		end
-	elseif RequiredScript == "lib/units/equipment/first_aid_kit/firstaidkitbase" then
-		function FirstAidKitBase.Add(obj, pos, min_distance)
-			local used_fak = ClientsidedUppers.GetUsedFirstAidKit(pos)
-
-			if used_fak then
-				used_fak:SyncUsage(obj._unit)
-				ClientsidedUppers.RemoveUsed(pos)
-			else
-				ClientsidedUppers.Remove(pos)
+		-- the syncrhonization part of taking a FAK stripped from the take function
+		function FirstAidKitBase:sync_usage()
+			if managers.network:session() then
+				managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "base", 2)
 			end
 
-			table.insert(
-				FirstAidKitBase.List,
-				{
-					obj = obj,
-					pos = pos,
-					min_distance = min_distance
-				}
-			)
+			self:_set_empty()
+		end
+		-- when the incoming FAK to be synchronized is owned by us we remove it's clientsided counterpart
+		-- when it was already used clientsided we synchronize it's usage
+		function FirstAidKitBase:sync_setup(bits, peer_id)
+			if Network:is_client() and peer_id == managers.network:session():local_peer():id() then
+				local fak = ClientsidedUppers.Remove(self)
+				if fak and fak._empty then
+					self:sync_usage()
+					return
+				end
+			end
+
+			if self._validate_clbk_id then
+				managers.enemy:remove_delayed_clbk(self._validate_clbk_id)
+
+				self._validate_clbk_id = nil
+			end
+
+			managers.player:verify_equipment(peer_id, "first_aid_kit")
+			self:setup(bits)
 		end
 	end
 end
